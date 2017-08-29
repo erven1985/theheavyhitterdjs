40095c6a84c88d800df43e19bfda208a4a4500bf
Yerlan <bayekeshov@gmail.com>
2017-08-29 00:24:41 -0400

    Alphabetical order

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -4,7 +4,7 @@ class ArtistsController < ApplicationController
   # GET /artists
   # GET /artists.json
   def index
-    @artists = Artist.all
+    @artists = Artist.order('name1 ASC')
   end
 
   # GET /artists/1
